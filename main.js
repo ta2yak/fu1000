c1fdfb2d3ef1093293c6e6185a6d293dd2993809
Tatsuya Kawasaki <ta2ya.xxx@gmail.com>
2017-05-29 00:52:45 +0900

    Simple Multi Window

--- main.js
+++ main.js
@@ -1,18 +1,55 @@
 const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const windowManager = require('electron-window-manager');
+const uuid = require('uuid');
+const {app, BrowserWindow, Menu, Tray} = electron;
 
 let mainWindow = null;
+let tray = null;
+let contextMenu = null;
 app.on('ready', () => {
-  // mainWindowを作成（windowの大きさや、Kioskモードにするかどうかなどもここで定義できる）
-  mainWindow = new BrowserWindow({width: 400, height: 300});
-  // Electronに表示するhtmlを絶対パスで指定（相対パスだと動かない）
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-
-  // ChromiumのDevツールを開く
-  mainWindow.webContents.openDevTools();
-
+  // **************************************************
+  // メインウィンドウの設定
+  // **************************************************
+  // メインウィンドウは表示しないため、最小化しておく
+  mainWindow = new BrowserWindow(
+    {
+      width: 1, 
+      height: 1, 
+      frame: false, 
+      show: false, 
+      "skip-taskbar": true
+    }
+  );
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
+
+  // **************************************************
+  // ウィンドウマネージャーの設定
+  // **************************************************
+  windowManager.setDefaultSetup({'width': 300, 'height': 200, frame: false});
+
+  // **************************************************
+  // タスクトレイの設定
+  // **************************************************
+  tray = new Tray(__dirname + "/public/images/icon.png");
+  contextMenu = Menu.buildFromTemplate([
+      { label: "新しい付箋を作成する", 
+        click: function () {
+          let id = uuid.v4();
+          windowManager.open(uuid.v4(), 'New Window', "file://" + __dirname + "/sticky/index.html" + "#" + id); 
+        } 
+      },
+      { label: "終了", 
+        click: function () { 
+          mainWindow.close(); 
+        } 
+      }
+  ]);
+  tray.setContextMenu(contextMenu);
+  tray.setToolTip(app.getName());
+  tray.on("clicked", function () {
+      tray.popUpContextMenu(contextMenu);
+  });
+
 });
